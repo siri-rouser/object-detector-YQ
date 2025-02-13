719ad53b7610b0ca8026d7c0993a81d1b8f06230
siri-rouser <yu.q.lin12@gmail.com>
2025-02-13 14:36:04 +0000

    usable arm64 dockerfile

--- trttest.py
+++ trttest.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
 
 # Load the YOLOv8 model
-model = YOLO('yolo11l.pt')
+model = YOLO('yolo11m.pt')
 
 # Export the model to TensorRT format
 model.export(
     format='engine', 
     device=0, 
-    imgsz=(1280,1280), 
+    imgsz=(640,640), 
     dynamic=True, 
     batch=1,
     half=True,
